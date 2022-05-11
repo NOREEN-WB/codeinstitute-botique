186a68f582c429bc23d230813044cd4e98246004
noreen-wb <nwbutt@hotmail.com>
2022-05-11 14:13:33 +0500

    home template

---
+++ home/views.py
@@ -0,0 +1,7 @@
+"""Imports"""
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    """view to return index.html"""
+    return render(request, 'home/index.html')
