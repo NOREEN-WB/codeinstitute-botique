c4ce75b403b89f9405c73c7fcf67ca2484d24c59
noreen-wb <nwbutt@hotmail.com>
2022-05-12 19:34:46 +0500

    media/fixtures/product/admin/models

---
+++ products/models.py
@@ -0,0 +1,37 @@
+"""imports"""
+from django.db import models
+
+
+class Category(models.Model):
+    """Category Model"""
+    class Meta:
+        """Meta Class"""
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        """name function"""
+        return self.name
+
+    def get_friendly_name(self):
+        """friendly function"""
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """products model"""
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        """return product name"""
+        return self.name
